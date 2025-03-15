f03369ff22cab8a620ed58f139e8b1377129dc86
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-03-15 11:41:04 +0500

    deploy logs improved

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -34,6 +34,11 @@ def run_remote_commands(commands: list[str]):
 def print_status(msg: str):
     print(f"\033[0;32m{msg}\033[0m")
 
+def print_warning(msg: str):
+    print(f"\033[0;33m{msg}\033[0m")
+
+def print_error(msg: str):
+    print(f"\033[0;31m{msg}\033[0m")
 
 def envsubst(from_file: str, to_file: str, variables: list[str] = []):
     if variables:
--- scripts/release.py
+++ scripts/release.py
@@ -1,14 +1,14 @@
 from scripts.http_request import request
-from scripts.helpers import print_status, save_env_option
+from scripts.helpers import print_status, save_env_option, run_command, print_warning
 from scripts.constants import (
     PROJECT_NAME, SENTRY_PROJECTS, SENTRY_RELEASE_TOKEN, SENTRY_URL, VERSION_FILE
 )
 
 def sentry_release(version: str):
-    print_status(f'Sending verion {version} to Sentry. Projects {SENTRY_PROJECTS.replace(",", ", ")}')
     if not SENTRY_RELEASE_TOKEN:
-        print_status('SENTRY_RELEASE_TOKEN is not set. Skipping Sentry release.')
+        print_warning('SENTRY_RELEASE_TOKEN is not set. Skipping Sentry release.')
         return
+    print_status(f'Sending verion {version} to Sentry. Projects {SENTRY_PROJECTS.replace(",", ", ")}')
     headers = {
         'Authorization': f'Bearer {SENTRY_RELEASE_TOKEN}',
     }
@@ -35,6 +35,7 @@ def save_version(version: str):
         f.write(version)
     save_env_option('PROJECT_VERSION', str(version), create=True)
     save_env_option('NEXT_PUBLIC_VERSION', str(version), create=True)
+    run_command(f'git commit environment/version -m "Version {version}"')
 
 def update_sentry_release():
     current_version = read_version()
